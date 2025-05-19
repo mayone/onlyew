7a230d37acfe5857b85ae1a01932a83a6451f863
Wayne Jeng <wayne.jeng@fortune.com.tw>
2025-05-19 17:54:15 +0800

    feat: tabs add indicator

--- src/components/tabs/mod.rs
+++ src/components/tabs/mod.rs
@@ -1,3 +1,4 @@
+use web_sys::HtmlElement;
 use yew::prelude::*;
 
 #[derive(Debug, PartialEq, Properties)]
@@ -21,7 +22,9 @@ pub enum TabsMessage {
 
 #[derive(Debug)]
 pub struct Tabs {
+    indicator_ref: NodeRef,
     selected_tab: usize,
+    tab_refs: Vec<NodeRef>,
 }
 
 impl Component for Tabs {
@@ -30,7 +33,17 @@ impl Component for Tabs {
 
     fn create(ctx: &Context<Self>) -> Self {
         let selected_tab = ctx.props().default_tab.unwrap_or(0);
-        Self { selected_tab }
+        let tab_refs = ctx
+            .props()
+            .children
+            .iter()
+            .map(|_| NodeRef::default())
+            .collect::<Vec<_>>();
+        Self {
+            indicator_ref: NodeRef::default(),
+            selected_tab,
+            tab_refs,
+        }
     }
 
     fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
@@ -56,35 +69,66 @@ impl Component for Tabs {
         } = ctx.props();
         let on_select = ctx.link().callback(TabsMessage::Select);
 
+        // let indicator_left = 100;
+        // let indicator_width = 100;
+
         html! {
             <div class={classes!("tabs", class.clone())} style={style.clone()}>
-                { for children.iter().enumerate().map(|(index, child)| {
-                    let is_selected = index == self.selected_tab;
-                    html! {
-                        <button
-                            class={classes!("tab-button", is_selected.then_some("selected"))}
-                            aria-selected={is_selected.to_string()}
-                            onclick={let on_select = on_select.clone();
-                                Callback::from(move |_| on_select.emit(index))}
-                        >
-                            { child }
-                        </button>
-                    }
-                })}
-                // { children.iter().enumerate().map(|(index, child)| {
-                //     let is_selected = index == self.selected_tab;
-                //     html! {
-                //         <button
-                //             class={classes!("tab-button", is_selected.then_some("selected"))}
-                //             aria-selected={is_selected.to_string()}
-                //             onclick={let on_select = on_select.clone();
-                //                 Callback::from(move |_| on_select.emit(index))}
-                //         >
-                //             { child }
-                //         </button>
-                //     }
-                // }).collect::<Vec<_>>()}
+                <div>
+                    { for children.iter().enumerate().map(|(index, child)| {
+                        let is_selected = index == self.selected_tab;
+                        html! {
+                            <button
+                                class={classes!("tab-button", is_selected.then_some("selected"))}
+                                aria-selected={is_selected.to_string()}
+                                ref={self.tab_refs[index].clone()}
+                                onclick={let on_select = on_select.clone();
+                                    Callback::from(move |_| on_select.emit(index))}
+                            >
+                                { child }
+                            </button>
+                        }
+                    }) }
+                    // { children.iter().enumerate().map(|(index, child)| {
+                    //     let is_selected = index == self.selected_tab;
+                    //     html! {
+                    //         <button
+                    //             class={classes!("tab-button", is_selected.then_some("selected"))}
+                    //             aria-selected={is_selected.to_string()}
+                    //             ref={self.tab_refs[index].clone()}
+                    //             onclick={let on_select = on_select.clone();
+                    //                 Callback::from(move |_| on_select.emit(index))}
+                    //         >
+                    //             { child }
+                    //         </button>
+                    //     }
+                    // }).collect::<Vec<_>>() }
+                </div>
+                <span class={classes!("tabs-indicator")} ref={self.indicator_ref.clone()} // style={format!("left: {}px; width: {}px", indicator_left, indicator_width)}
+                />
             </div>
+        }
+    }
+
+    fn rendered(&mut self, _ctx: &Context<Self>, _first_render: bool) {
+        let indicator = self.indicator_ref.cast::<HtmlElement>().unwrap();
+
+        if let Some(element) = self.tab_refs[self.selected_tab].cast::<HtmlElement>() {
+            // let indicator_style = format!(
+            //     "height: {}px; width: {}px; \
+            //                         transform: translateX({}px) translateY({}px);",
+            //     element.client_height(),
+            //     element.client_width(),
+            //     element.offset_left(),
+            //     element.offset_top(),
+            // );
+            let indicator_style = format!(
+                "width: {}px; \
+                                    transform: translateX({}px) ",
+                element.client_width(),
+                element.offset_left(),
+            );
+            let _ = indicator.set_attribute("style", &indicator_style);
         }
     }
 }
@@ -95,6 +139,6 @@ mod tests {
 
     #[test]
     fn test_render_tabs() {
-        let _ = html! { <Tabs>{"Tab 1"}</Tabs> };
+        let _ = html! { <Tabs>{ "Tab 1" }</Tabs> };
     }
 }
