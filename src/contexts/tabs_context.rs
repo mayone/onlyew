f6204607f97c4ae6a763170a0b35cb6f47d29bc6
Wayne Jeng <wayne.jeng@fortune.com.tw>
2025-06-05 17:53:51 +0800

    todo: add tab context to be finished later

---
+++ src/contexts/tabs_context.rs
@@ -0,0 +1,44 @@
+use std::rc::Rc;
+
+use yew::prelude::*;
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Tabs {
+    pub selected_tab: AttrValue,
+}
+
+#[derive(Debug)]
+pub enum TabsAction {
+    Select(AttrValue),
+}
+
+impl Reducible for Tabs {
+    type Action = TabsAction;
+
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        match action {
+            Self::Action::Select(value) => Rc::new(Self {
+                selected_tab: value,
+            }),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Properties)]
+pub struct TabsProviderProperties {
+    #[prop_or_default]
+    pub children: Children,
+}
+
+#[function_component]
+pub fn TabsProvider(props: &TabsProviderProperties) -> Html {
+    let tabs = use_reducer(|| Tabs {
+        selected_tab: "0".into(),
+    });
+
+    html! {
+        <ContextProvider<TabsContext> context={()}>
+            { props.children.clone() }
+        </ContextProvider<TabsContext>>
+    }
+}
